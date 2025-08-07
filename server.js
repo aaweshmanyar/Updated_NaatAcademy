8111c93be7d3399ed3a6fd10ea77045c393225be
Niraj240404 <baviskarniraj1@gmail.com>
2025-08-07 17:01:09 +0530

    new

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const corsOptions = {
             'http://127.0.0.1:5503',          // Localhost (127)
             'http://localhost:5000',
             'https://naatacadmeyadmin.vercel.app',
-            'http://localhost:5173/'           // Localhost (localhost)
+            'http://localhost:5173'           // Localhost (localhost)
         ];
         
         // Allow requests with no origin (like mobile apps, curl requests)
